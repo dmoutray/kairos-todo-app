53b6a9dfaf1e7ab4ca35e0d1a26be4543cc5b8e2
David Moutray <dmoutray@live.co.uk>
2020-06-13 10:28:57 +0100

    Add some boilerplate code.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import {
+    HashRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom'
 import './App.css';
+import Home from "./views/home";
+import Login from "./views/login";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+
+            <Router>
+                {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+                <Switch>
+                    <Route path="/todo">
+                        <Home/>
+                    </Route>
+                    <Route path="/login">
+                        <Login/>
+                    </Route>
+                </Switch>
+
+            </Router>
+
+        </div>
+    );
 }
 
 export default App;
