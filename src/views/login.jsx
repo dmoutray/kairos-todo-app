32bd771d19cd0204e5d0c26fb584d5964c9f1d98
David Moutray <dmoutray@live.co.uk>
2020-06-15 16:17:19 +0100

    Add login component

--- src/views/login.jsx
+++ src/views/login.jsx
@@ -1,19 +1,53 @@
 import React from "react";
 
+import Notification from "../components/notification";
+
 export default class Login extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            userName: '',
+            error: {
+                status: false,
+                message: ''
+            }
         }
+    }
 
+    handleChange = (event) => {
+        this.setState({
+            userName: event.target.value
+        })
+    }
+
+    validate = () => {
+        if (this.state.userName) {
+            sessionStorage.setItem('userName', this.state.userName)
+            this.props.history.push('/todo')
+        } else {
+            this.setState({
+                error: {status: true, message: 'Username cannot be empty'}
+            }, () => {
+                setTimeout(() => {
+                    this.setState({
+                        error: {...this.state.error, status: false}
+                    },)
+                }, 5000)
+            })
+        }
     }
 
 
     render() {
         return (
-            <div>
-
+            <div className={'login-container'}>
+                <div><h1>Todo App</h1></div>
+                <Notification error={this.state.error}/>
+                <div className={'login'}>
+                    <input type={'text'} onChange={this.handleChange} placeholder={'Enter a username...'}/>
+                    <button className={'button primary'} onClick={this.validate}>Login</button>
+                </div>
             </div>
         )
     }
