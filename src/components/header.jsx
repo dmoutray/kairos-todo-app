809395116e9a5a2d73ed698f57b06441ef203947
David Moutray <dmoutray@live.co.uk>
2020-06-15 16:13:52 +0100

    Add header

---
+++ src/components/header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import {useHistory} from "react-router-dom"
+
+export default function Header(props) {
+
+    const reactHistory = useHistory()
+
+    return (
+        <div className='header'>
+            <div>Todo App</div>
+            <div>{'Welcome, ' + props.userName}
+                <button className={'button primary'} onClick={
+                    function () {
+                        sessionStorage.setItem('userName', '')
+                        reactHistory.push('/login')
+                    }
+                }>Logout
+                </button>
+            </div>
+        </div>
+    )
+}
