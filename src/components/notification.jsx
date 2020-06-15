2cca33eda32f4b08ad800a0e0f17bdf63a22c314
David Moutray <dmoutray@live.co.uk>
2020-06-15 16:18:21 +0100

    Add notification system

---
+++ src/components/notification.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Notification(props) {
+    return (
+        <div className={'notification-container'}>
+            <div className={'notification'} style={
+                {
+                    top: props.error.status ? '0' : '-200px',
+                    transition: 'top 0.8s ease-in-out',
+                }
+            }>
+                {props.error.message}
+            </div>
+        </div>
+    )
+}
